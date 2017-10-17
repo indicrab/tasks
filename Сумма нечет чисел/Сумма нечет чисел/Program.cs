309167813aedd563510b9d1ae81d652620151124
indicrab <nikish.p@gmail.com>
2017-10-17 16:56:15 +0300

    Upload

---
+++ Сумма нечет чисел/Сумма нечет чисел/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сумма_нечет_чисел
+{
+    class Program
+    {
+        //С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+        //Требуется подсчитать сумму всех нечетных положительных чисел.Сами числа и сумму вывести на экран;
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int summ = 0;
+            while (a != 0)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a % 2 != 0) summ = summ + a;
+            }
+            Console.WriteLine(summ);
+
+            Console.ReadKey();
+        }
+    }
+}
