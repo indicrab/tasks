45488f13aa1d22247620ad20fba4c837fdaf5ec0
indicrab <nikish.p@gmail.com>
2017-10-27 10:09:53 +0300

    new

---
+++ LogincheckRegex/LogincheckRegex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LogincheckRegex
+{
+    //Создать программу, которая будет проверять корректность ввода логина. 
+    //Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы и цифры, 
+    //и при этом цифра не может быть первой.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login");
+            var log = Console.ReadLine();
+            LoginTest(log);
+            Console.ReadKey();
+            
+        }
+        static void LoginTest(string login)
+        {
+
+            var reg = new Regex(@"^[^0-9]\w{1,10}$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+            if (reg.IsMatch(login))
+            {
+                Console.WriteLine("login is correct");
+            }
+            else
+            {
+                Console.WriteLine("wrong login");
+            }
+        }
+    }
+}
---
+++ password complexity/password complexity/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace password_complexity
+{
+    class Program
+    {
+        //создать программу, которая проверяет сложность пароля от 4 до 16 имсволов
+        //легкий пароль, только симолы или только цифры
+        //средний пароль, символы  и цифры 
+        //сложный пароль символ верхнего и нижнего регистра, цифры и симолвы
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+            Pass(password);
+            Console.ReadKey();
+        }
+        
+        static void Pass(string pas)
+        {
+            var low = new Regex(@"^[a-z] | [0-9]{3,16}$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+            var mid = new Regex(@"^[A-z0-9]{3,16}$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+            var hard = new Regex(@"^[A-z0-9]{3,16}\!@#$%^&*()_+$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+            if (low.IsMatch(pas))
+            {
+                Console.WriteLine("low complexity");
+            }
+            else if (mid.IsMatch(pas))
+            {
+                Console.WriteLine("mid complexity");
+            }
+            else if (hard.IsMatch(pas))
+            {
+                Console.WriteLine("hard complexity");
+            }
+        }
+        
+    }
+}
